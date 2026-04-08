c53f8c894977201eac4642b0cc3bf811f725cfb5
Solijon <solijonikromov001@gmail.com>
2026-04-08 15:59:19 +0500

    Add server to main branch with open CORS

--- server/index.js
+++ server/index.js
@@ -5,15 +5,7 @@ const contractRouter = require('./routes/contract')
 
 const app = express()
 
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:4173',
-    /\.vercel\.app$/,
-    process.env.FRONTEND_URL,
-  ].filter(Boolean),
-  methods: ['GET', 'POST'],
-}))
+app.use(cors())
 
 // Base64 image uchun limit oshiriladi
 app.use(express.json({ limit: '50mb' }))
